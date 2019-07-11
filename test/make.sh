8a6e2800569a67f00b375ac3cff9bfad4b10d81a
Jeff McCune <jeff@openinfrastructure.co>
2019-07-11 09:23:35 -0700

    Add TF 0.12 compatiblity to make check_terraform

--- test/make.sh
+++ test/make.sh
@@ -35,7 +35,12 @@ maketemp() {
 find_files() {
   local pth="$1"
   shift
-  find "${pth}" '(' -path '*/.git' -o -path '*/.terraform' ')' \
+  # Note: Take care to use -print or -print0 when using this function,
+  # otherwise excluded directories will be included in the output.
+  find "${pth}" '(' \
+    -path '*/.git' -o \
+    -path '*/.terraform' -o \
+    -path '*/.kitchen' ')' \
     -prune -o -type f "$@"
 }
 
@@ -70,20 +75,31 @@ function docker() {
     | compat_xargs -0 hadolint
 }
 
-# This function runs 'terraform validate' and 'terraform fmt'
-# against all directory paths which contain *.tf files.
+# This function runs 'terraform validate' against all
+# directory paths which contain *.tf files.
 function check_terraform() {
   set -e
-  echo "Running terraform validate"
-  find_files . -name "*.tf" -print0 \
-    | compat_xargs -0 -n1 dirname \
-    | sort -u \
-    | compat_xargs -t -n1 terraform validate --check-variables=false
+  # fmt is before validate for faster feedback, validate requires terraform
+  # init which takes time.
   echo "Running terraform fmt"
   find_files . -name "*.tf" -print0 \
     | compat_xargs -0 -n1 dirname \
     | sort -u \
-    | compat_xargs -t -n1 terraform fmt -check=true -write=false
+    | compat_xargs -t -n1 terraform fmt -diff -check=true -write=false
+  rval="$?"
+  if [[ "${rval}" -gt 0 ]]; then
+    echo "Error: terraform fmt failed with exit code ${rval}" >&2
+    echo "Check the output for diffs and correct using terraform fmt <dir>" >&2
+    return "${rval}"
+  fi
+  echo "Running terraform validate"
+  # Change to a temporary directory to avoid re-initializing terraform init
+  # over and over in the root of the repository.
+  find_files . -name "*.tf" -print \
+    | grep -v 'test/fixtures/shared' \
+    | compat_xargs -n1 dirname \
+    | sort -u \
+    | compat_xargs -t -n1 test/terraform_validate
 }
 
 # This function runs 'go fmt' and 'go vet' on every file
