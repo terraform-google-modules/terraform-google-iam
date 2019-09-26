8cae73c6f150f289ae426f0166e9f22a1e47a387
Pavel Zhukov <cray0000@gmail.com>
2019-09-26 20:46:03 +0300

    Fix buckets IAM in authoritative mode

--- modules/storage_buckets_iam/main.tf
+++ modules/storage_buckets_iam/main.tf
@@ -21,31 +21,50 @@ locals {
   authoritative        = var.mode == "authoritative" ? 1 : 0
   additive             = var.mode == "additive" ? 1 : 0
   storage_bucket_count = var.storage_buckets_num == 0 ? length(var.storage_buckets) : var.storage_buckets_num
-  bindings_formatted   = distinct(flatten([for storage_bucket in var.storage_buckets : [for value in flatten([for k, v in var.bindings : [for val in v : { "role_name" = k, "member_id" = val }]]) : merge({ "storage_bucket_name" = storage_bucket }, value)]]))
+
+  bindings_by_role     = distinct(flatten([
+    for bucket in var.storage_buckets
+    : [
+      for role, members in var.bindings
+      : { role = role, members = members, bucket = bucket }
+    ]
+  ]))
+
+  bindings_by_member   = distinct(flatten([
+    for binding in local.bindings_by_role
+    : [
+      for member in binding["members"]
+      : { bucket = binding["bucket"], role = binding["role"], member = member }
+    ]
+  ]))
 }
 
 /******************************************
   Storage Bucket IAM binding authoritative
  *****************************************/
 resource "google_storage_bucket_iam_binding" "storage_bucket_iam_authoritative" {
-  count = var.bindings_num > 0 ? var.bindings_num * local.authoritative : length(distinct(local.bindings_formatted[*].role_name)) * local.authoritative * local.storage_bucket_count
+  count = local.authoritative * (
+    var.bindings_num > 0
+      ? var.bindings_num * local.storage_bucket_count
+      : length(local.bindings_by_role)
+  )
 
-  bucket = local.bindings_formatted[count.index].storage_bucket_name
-  role   = local.bindings_formatted[count.index].role_name
-  members = [
-    for binded in local.bindings_formatted :
-    binded.member_id if binded.storage_bucket_name == local.bindings_formatted[count.index].storage_bucket_name && binded.role_name == local.bindings_formatted[count.index].role_name
-  ]
+  bucket  = local.bindings_by_role[count.index].bucket
+  role    = local.bindings_by_role[count.index].role
+  members = local.bindings_by_role[count.index].members
 }
 
 /******************************************
   Storage Bucket IAM binding additive
  *****************************************/
 resource "google_storage_bucket_iam_member" "storage_bucket_iam_additive" {
-  count = var.bindings_num > 0 ? var.bindings_num * local.additive * local.storage_bucket_count : length(local.bindings_formatted) * local.additive
+  count = local.additive * (
+    var.bindings_num > 0
+      ? var.bindings_num * local.storage_bucket_count
+      : length(local.bindings_by_member)
+  )
 
-  bucket = local.bindings_formatted[count.index].storage_bucket_name
-  role   = local.bindings_formatted[count.index].role_name
-  member = local.bindings_formatted[count.index].member_id
+  bucket = local.bindings_by_member[count.index].bucket
+  role   = local.bindings_by_member[count.index].role
+  member = local.bindings_by_member[count.index].member
 }
-
