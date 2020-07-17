8ae9174eaa0a4398da9a94eab735763656931546
Fraj KALLEL <fraj@skale-5.com>
2020-07-17 19:54:12 +0200

    update prefix

--- examples/member_iam/main.tf
+++ examples/member_iam/main.tf
@@ -36,5 +36,5 @@ module "member_roles" {
   service_account_address = google_service_account.member_iam_test.email
   project_id              = var.project_id
   project_roles           = ["roles/compute.networkAdmin", "roles/appengine.appAdmin"]
-  prefix                  = "group"
+  prefix                  = "serviceAccount"
 }
