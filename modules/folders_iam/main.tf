747652e9d14db2516cae69fea81ac15c9d07b064
Rahul Gupta <guptarhl@google.com>
2020-08-26 15:16:47 -0700

    adding conditions to folders_iam

--- modules/folders_iam/main.tf
+++ modules/folders_iam/main.tf
@@ -32,6 +32,14 @@ resource "google_folder_iam_binding" "folder_iam_authoritative" {
   folder   = "folders/${replace(module.helper.bindings_authoritative[each.key].name, "folders/", "")}"
   role     = module.helper.bindings_authoritative[each.key].role
   members  = module.helper.bindings_authoritative[each.key].members
+  dynamic "condition" {
+    for_each = module.helper.bindings_authoritative[each.key].condition.title == "" ? [] : [module.helper.bindings_authoritative[each.key].condition]
+    content {
+      title       = module.helper.bindings_authoritative[each.key].condition.title
+      description = module.helper.bindings_authoritative[each.key].condition.description
+      expression  = module.helper.bindings_authoritative[each.key].condition.expression
+    }
+  }
 }
 
 /******************************************
@@ -42,4 +50,12 @@ resource "google_folder_iam_member" "folder_iam_additive" {
   folder   = "folders/${replace(module.helper.bindings_additive[each.key].name, "folders/", "")}"
   role     = module.helper.bindings_additive[each.key].role
   member   = module.helper.bindings_additive[each.key].member
+  dynamic "condition" {
+    for_each = module.helper.bindings_additive[each.key].condition.title == "" ? [] : [module.helper.bindings_additive[each.key].condition]
+    content {
+      title       = module.helper.bindings_additive[each.key].condition.title
+      description = module.helper.bindings_additive[each.key].condition.description
+      expression  = module.helper.bindings_additive[each.key].condition.expression
+    }
+  }
 }
