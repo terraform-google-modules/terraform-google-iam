f54ebeedd3fee9c90adf06046900396c901beff2
Rahul Gupta <guptarhl@google.com>
2020-08-26 16:22:00 -0700

    new example for conditional project iam

---
+++ examples/project_conditions/main.tf
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Provider configuration
+ *****************************************/
+provider "google" {
+  version = "~> 3.33"
+}
+
+provider "google-beta" {
+  version = "~> 3.36"
+}
+
+/******************************************
+  Module project_iam_binding calling
+ *****************************************/
+module "project_iam_binding" {
+  source   = "../../modules/projects_iam/"
+  projects = [var.project_one, var.project_two]
+  mode     = "additive"
+
+  bindings = {
+    "roles/compute.networkAdmin" = [
+      "serviceAccount:${var.sa_email}",
+      "group:${var.group_email}",
+      "user:${var.user_email}",
+    ]
+    "roles/appengine.appAdmin" = [
+      "serviceAccount:${var.sa_email}",
+      "group:${var.group_email}",
+      "user:${var.user_email}",
+    ]
+  }
+
+  conditional_bindings = [
+    {
+      role        = "roles/storage.Admin"
+      title       = "expires_after_2019_12_31"
+      description = "Expiring at midnight of 2019-12-31"
+      expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+      members = [
+        "serviceAccount:${var.sa_email}",
+        "group:${var.group_email}",
+        "user:${var.user_email}",
+      ]
+    }
+  ]
+}
+
---
+++ examples/project_conditions/variables.tf
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "group_email" {
+  type        = string
+  description = "Email for group to receive roles (ex. group@example.com)"
+}
+
+variable "sa_email" {
+  type        = string
+  description = "Email for Service Account to receive roles (Ex. default-sa@example-project-id.iam.gserviceaccount.com)"
+}
+
+variable "user_email" {
+  type        = string
+  description = "Email for group to receive roles (Ex. user@example.com)"
+}
+
+/******************************************
+  project_iam_binding variables
+ *****************************************/
+variable "project_one" {
+  type        = string
+  description = "First project id to add the IAM policies/bindings"
+}
+
+variable "project_two" {
+  type        = string
+  description = "Second project id to add the IAM policies/bindings"
+}
+
