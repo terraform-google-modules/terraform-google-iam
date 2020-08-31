97d49d68e48386a8d05e1f5677b801ae9e5175e5
Rahul Gupta <guptarhl@google.com>
2020-08-30 17:59:17 -0700

    adding empty default for bindings vr

--- modules/helper/variables.tf
+++ modules/helper/variables.tf
@@ -17,6 +17,7 @@
 variable "bindings" {
   description = "Map of role (key) and list of members (value) to add the IAM policies/bindings"
   type        = map(list(string))
+  default     = {}
 }
 
 variable "mode" {
--- modules/service_accounts_iam/variables.tf
+++ modules/service_accounts_iam/variables.tf
@@ -34,6 +34,7 @@ variable "mode" {
 variable "bindings" {
   description = "Map of role (key) and list of members (value) to add the IAM policies/bindings"
   type        = map(list(string))
+  default     = {}
 }
 
 variable "conditional_bindings" {
