a0c07c35cfe029c699396f64da858473a45492a1
Morgante Pell <morgantep@google.com>
2021-03-09 19:30:10 -0500

    fix: Correctly exclude unsupported permissions at all stages. (#131)

--- modules/custom_role_iam/main.tf
+++ modules/custom_role_iam/main.tf
@@ -34,6 +34,7 @@ data "google_iam_role" "role_permissions" {
  *****************************************/
 data "google_iam_testable_permissions" "unsupported_permissions" {
   full_resource_name   = var.target_level == "org" ? "//cloudresourcemanager.googleapis.com/organizations/${var.target_id}" : "//cloudresourcemanager.googleapis.com/projects/${var.target_id}"
+  stages               = ["GA", "ALPHA", "BETA"]
   custom_support_level = "NOT_SUPPORTED"
 }
 
