8f3ca63e02e5a718396d87f13bf137d2dcc3e7ea
filip-kaminski <22922195+filip-kaminski@users.noreply.github.com>
2025-12-14 14:34:01 +0100

    feat: added Cloud Run V2 services iam submodule

---
+++ examples/cloud_run_v2_service/README.md
@@ -0,0 +1,22 @@
+# Cloud Run Example
+
+This example illustrates how to use the `cloud_run_service_iam` submodule
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| cloud\_run\_service\_location | The location of the cloud run instance | `string` | n/a | yes |
+| cloud\_run\_service\_one | First cloud run service to add the IAM policies/bindings | `string` | n/a | yes |
+| cloud\_run\_service\_project | Project id of the cloud run service | `string` | n/a | yes |
+| cloud\_run\_service\_two | Second cloud run service to add the IAM policies/bindings | `string` | n/a | yes |
+| group\_email | Email for group to receive roles (ex. group@example.com) | `string` | n/a | yes |
+| sa\_email | Email for Service Account to receive roles (Ex. default-sa@example-project-id.iam.gserviceaccount.com) | `string` | n/a | yes |
+| user\_email | Email for group to receive roles (Ex. user@example.com) | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ modules/cloud_run_v2_services_iam/variables.tf
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project" {
+  description = "Project to add the IAM policies/bindings"
+  default     = ""
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the cloud run instance"
+  default     = ""
+  type        = string
+}
+
+variable "cloud_run_services" {
+  description = "Cloud Run services list to add the IAM policies/bindings"
+  default     = []
+  type        = list(string)
+}
+
+variable "mode" {
+  description = "Mode for adding the IAM policies/bindings, additive and authoritative"
+  type        = string
+  default     = "additive"
+}
+
+variable "bindings" {
+  description = "Map of role (key) and list of members (value) to add the IAM policies/bindings"
+  type        = map(any)
+}
